9e8b22bf83a56117f2ab242fb453678f9c5e7ad7
AlShahadb5 <k20067364@kcl.ac.uk>
2022-12-09 03:38:28 +0000

    Employe2

--- employee.py
+++ employee.py
@@ -79,16 +79,16 @@ class Employee:
 billie = Employee('Billie', "monthly", 4000, 0, False, "None", 0, 0)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', "hourly", 100, 25, False, "None", 0, 0)
+charlie = Employee('Charlie', 'hourly', 100, 25, False, "None", 0, 0)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee', "monthly", 3000, 0 , True, "contract", 200, 4)
+renee = Employee('Renee', 'monthly', 3000, 0 , True, 'contract', 200, 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan', "hourly", 150, 25, True, "contract", 220, 3)
+jan = Employee('Jan', 'hourly', 150, 25, True, "contract", 220, 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie', "monthly", 2000, 0, True, "bonus", 1500, 0)
+robbie = Employee('Robbie', 'monthly', 2000, 0, True, "bonus", 1500, 0)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel', "hourly", 120, 30, True, "bonus", 600, 0)
+ariel = Employee('Ariel', 'hourly', 120, 30, True, "bonus", 600, 0)
